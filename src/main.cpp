e5396ca70a1e3940a458513536f3ae95fb06142e
joelmorera <joel.joan.morera@estudiantat.upc.edu>
2023-03-02 18:15:21 +0100

    Add P2.A to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,42 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+struct Button 
+{
+    const uint8_t PIN;
+    uint32_t numberKeyPresses;
+    bool pressed;
+};
+
+
+void IRAM_ATTR isr()
+{
+    button1.numberKeyPresses += 1;
+    button1.pressed = true;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+Button button1 = {18, 0, false};
+static uint32_t lastMillis = 0;
+
+
+void setup() 
+{
+    Serial.begin(115200);
+    pinMode(button1.PIN, INPUT_PULLUP);
+    attachInterrupt(button1.PIN, isr, FALLING);
 }
+void loop() {
+    if (button1.pressed) 
+    {
+        Serial.printf("Button 1 has been pressed %u times\n", button1.numberKeyPresses);
+        button1.pressed = false;
+    }
+
+    //Detach Interrupt after 1 Minute
+    if (millis() - lastMillis > 60000) 
+    {
+        detachInterrupt(button1.PIN);
+        Serial.println("Interrupt Detached!");
+    }
+
+    lastMillis = millis();
+} 
