dd45c76eb2c21c4b74ca39243e9eb61db5349b9b
joelmorera <joel.joan.morera@estudiantat.upc.edu>
2023-03-02 20:30:18 +0100

    Add final practice to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,31 @@ volatile int interruptCounter;
 int totalInterruptCounter;
 hw_timer_t * timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+bool IN,vact,vant = 0,cama = 0,cam,orden = 0;
 
 void IRAM_ATTR onTimer()
 {
     portENTER_CRITICAL_ISR(&timerMux);
     interruptCounter++;
     portEXIT_CRITICAL_ISR(&timerMux);
+    
+    
+    //Filter
+
+    vact = digitalRead(4);
+
+    cam = vant ^ vact;
+
+    if (cam == 1 && cama == 1)
+    {
+      IN = digitalRead(18);
+      vant = vact;
+      orden = 1;
+      cama = 0;
+      digitalWrite(18,vact);
+      return;
+    }
+    cama = cam;
 }
 
 void setup() 
@@ -17,8 +36,10 @@ void setup()
     Serial.begin(115200);
     timer = timerBegin(0, 80, true);
     timerAttachInterrupt(timer, &onTimer, true);
-    timerAlarmWrite(timer, 1000000, true);
+    timerAlarmWrite(timer, 10000, true);
     timerAlarmEnable(timer);
+    pinMode(18,OUTPUT);
+    pinMode(4,INPUT_PULLUP);
 }
 
 void loop() 
@@ -30,8 +51,12 @@ void loop()
         portEXIT_CRITICAL(&timerMux);
         totalInterruptCounter++;
         
-        Serial.print("An interrupt has occurred. Total number: ");
-        Serial.println(totalInterruptCounter);
+        //Serial.print("An interrupt has occurred. Total number: ");
+        //Serial.println(totalInterruptCounter);
+        if (orden)
+        {
+          Serial.println("boton!");
+          orden = 0;
+        }
     }
 }
-
